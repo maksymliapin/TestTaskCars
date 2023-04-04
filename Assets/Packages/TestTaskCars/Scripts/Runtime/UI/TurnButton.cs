f42cb5094ee565223747c603c8d3af046548a41c
Maksym Liapin <maksym.liapin@bini.games>
2023-04-04 16:20:52 +0300

    Added buttons

--- Assets/Packages/TestTaskCars/Scripts/Runtime/UI/TurnButton.cs
+++ Assets/Packages/TestTaskCars/Scripts/Runtime/UI/TurnButton.cs
@@ -1,4 +1,5 @@
-﻿using DG.Tweening;
+﻿using System;
+using DG.Tweening;
 using DG.Tweening.Core;
 using DG.Tweening.Plugins.Options;
 using Packages.TestTaskCars.Scripts.Runtime.Cars;
@@ -11,23 +12,40 @@ namespace Packages.TestTaskCars.Scripts.Runtime.UI
         [SerializeField] private bool Isleft;
         [SerializeField] private CarMover car;
         [SerializeField] private Transform steeringWheel;
-        private TweenerCore<Quaternion,Vector3,QuaternionOptions> tween;
+        [SerializeField] private SpriteRenderer carRenderer;
+        [SerializeField] private Transform leftBord;
+        [SerializeField] private Transform rightBord;
+        private TweenerCore<Quaternion, Vector3, QuaternionOptions> tween;
+        private float carSizeX;
+
+        private void Awake() => 
+            carSizeX = carRenderer.bounds.size.x/2;
 
         private void OnMouseDrag()
         {
             if (Isleft)
             {
-                car.TurnLeft();
+                var offset = leftBord.position.x + carSizeX;
+                if (car.transform.position.x > offset)
+                {
+                    car.TurnLeft();
+                    carRenderer.transform.DOLocalRotate(new Vector3(90, 0, 5), .5f);
+                }
             }
             else
             {
-                car.TurnRight();
+                var offset = rightBord.position.x - carSizeX;
+                if (car.transform.position.x <offset)
+                {
+                    car.TurnRight();
+                    carRenderer.transform.DOLocalRotate(new Vector3(90, 0, -5), .5f);
+                }
             }
         }
 
         private void OnMouseDown()
         {
-           tween = steeringWheel.DOLocalRotate(
+            tween = steeringWheel.DOLocalRotate(
                 Isleft ? new Vector3(0, 0, 60) : new Vector3(0, 0, -60), 1);
         }
 
@@ -35,6 +53,7 @@ namespace Packages.TestTaskCars.Scripts.Runtime.UI
         {
             tween.Kill();
             steeringWheel.DOLocalRotate(Vector3.zero, .5f);
+            carRenderer.transform.DOLocalRotate(new Vector3(90, 0, 0), .5f);
         }
     }
 }
